8a5d606e01e8196a6d01bf2de4c887c60d8adec9
dalyIsaac <isaac.daly.dev@outlook.com>
2017-12-04 13:38:09 +1300

    EEPROM tests

---
+++ aardvark/aardvark_settings.py
@@ -0,0 +1,7 @@
+"""Contains the settings for the tests which uses the Aardvark API"""
+
+PORT_NUMBER = 0
+PAGE_SIZE = 16 # NOTE: The number is the number of bytes, per documentation
+PAGE_SIZE.__doc__ = "PAGE_SIZE is the number of bytes within a page."
+NUM_PAGES = 32 # per documentation
+SLAVE_ADDRESS = 0

9f42bc07590b2c6b36b40b13d3ecb70bb2aae8a8
dalyIsaac <isaac.daly@gmail.com>
2018-01-23 11:11:41 +1300

    Updated documentation, and removed superfluous files

--- test_harness.py
+++ test_harness.py
@@ -26,15 +26,15 @@ def main():
         rtc_main()
     if COMMS_TESTS:
         # NOTE: Runs on the PIM Mini
-        from comms.comms import main as comms_main
+        from comms_controller import main as comms_main
         comms_main()
     if LEDS_TESTS:
         # NOTE: Runs on the PIM Mini
-        from leds.leds import main as leds_main
+        from leds_controller import main as leds_main
         leds_main()
     if USER_INPUTS_TESTS:
         # NOTE: Runs on the PIM Mini
-        from user_inputs.user_inputs import main as user_inputs_main
+        from user_inputs_controller import main as user_inputs_main
         user_inputs_main()
 
     print "\n\n\n"
